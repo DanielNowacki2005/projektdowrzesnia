ff179fdfd18c2f3af05d838586ace39d59a320c1
DanielNowacki2005 <144656552+DanielNowacki2005@users.noreply.github.com>
2023-09-18 09:23:20 +0200

    aplication
    yes only one

---
+++ demoo8i87875875/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.demoo8i87875875 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.demoo8i87875875 to javafx.fxml;
+    exports com.example.demoo8i87875875;
+}
